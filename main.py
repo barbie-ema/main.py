62619ca1456a17f7dfeafd940d4f3e7fd4c39352
barbie-ema <105289484+barbie-ema@users.noreply.github.com>
2022-06-09 22:25:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ opt = {'R':'Rock',
 'S': 'Scissors', 'P':'Paper'}
 
 while True:
+  
   user_input = input("Enter a choice R, P, S: ")
   computer_action = ["R", "P", "S"]
   computer_choice = random.choice(computer_action)
-  print(f"\nPlayer's choice {user_input}, CPU choice {computer_choice}.\n")
-
+  if user_input not in (opt):
+            
+            print('Invalid input! Please enter a valid input')
+            continue
+  
+  print(f"Player's choice ({opt.get(user_input)}) : CPU choice ({opt.get(computer_choice)})")
+  
+  
   if user_input == computer_choice:
     print(f"Both players selected {user_input}. It's a tie!","\U0001F600" )
     continue
@@ -19,20 +26,19 @@ while True:
         print("Rock smashes scissors! You win!" ,"\U0001F60D")
         break
     else:
-        print("Paper covers rock! You lose." ,"\U0001F605")
+        print("Paper covers rock! CPU wins! You lose." ,"\U0001F605")
   elif user_input == "P":
     if computer_choice == "R)":
         print("Paper covers rock! You win!","\U0001F60D" )
         break
     else:
-        print("Scissors cuts paper! You lose.", "\U0001F605")
+        print("Scissors cuts paper! CPU wins! You lose.", "\U0001F605")
   elif user_input == "S":
     if computer_choice == "P)":
         print("Scissors cuts paper! You win!","\U0001F605" )
         break
     else:
-        print("Rock smashes scissors! You lose.","\U0001F605")
-
+        print("Rock smashes scissors! CPU wins! You lose.","\U0001F605")
   print("Do you want to play again? (Y/N)","\U0001F607")
   ans = input ()
  
@@ -40,7 +46,8 @@ while True:
     
   if ans == 'n' or ans == 'N':
       break
+   
 
-
+print("\nThanks for playing")
 print("\nThanks for playing", "\U0001F60D")
         
